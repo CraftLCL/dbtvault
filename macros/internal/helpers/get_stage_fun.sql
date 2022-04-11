83542de9bde5a4ee0810a69b6c69617a090c6daf
GawainLi <lcl246879513@outlook.com>
2022-04-11 14:47:21 +0800

    1.stage fun add

---
+++ macros/internal/helpers/get_stage_fun.sql
@@ -0,0 +1,15 @@
+{%- macro get_load_datetime() -%}
+    to_timestamp(cast(ts as float) / 1000) at time zone 'Asia/Shanghai' 
+{%- endmacro -%}
+
+{%- macro get_effective_from() -%}
+    to_timestamp(cast(ts as float) / 1000) at time zone 'Asia/Shanghai'
+{%- endmacro -%}
+
+{%- macro get_start_date(default) -%}
+    case when ts = {{default}} then '1999-1-01 1:00:00.000' else to_timestamp(cast(ts as float) / 1000) at time zone 'Asia/Shanghai' end
+{%- endmacro -%}
+
+{%- macro get_end_date() -%}
+    coalesce(to_timestamp(cast(lead(ts, 1, null) over ( partition by id order by ts) as float) / 1000) at time zone 'Asia/Shanghai','9999-12-31 23:59:59.999999')
+{%- endmacro -%}
